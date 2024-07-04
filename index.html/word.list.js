7885c3c3259ff7b8ac8dd376a14f9b92ce624b7a
ChunHan08 <che1208@hwdsb.on.ca>
2024-07-03 21:34:53 -0400

    first commit

---
+++ index.html/word.list.js
@@ -0,0 +1,114 @@
+const wordList = [
+    {
+      word: "symphony",
+      hint: "A long musical composition for a full orchestra, typically in multiple movements."
+    },
+    { 
+      word: "adventure",
+      hint: "An exciting or daring experience."
+    },
+    {
+    word: "telephone",
+    hint: "A device used to transmit sound over long distances."
+    },
+    {
+    word: "sunset",
+    hint: "The daily disappearance of the sun below the horizon."
+    },
+    {
+    word: "shadow",
+    hint: "A dark area or shape produced by an object blocking the light."
+    },
+    {
+    word: "chocolate",
+    hint: "A sweet treat made from cocoa beans."
+    },
+    {
+    word: "vibrant",
+    hint: "Full of energy, brightnes, and life."
+    },
+    {
+    word: "illusion",
+    hint: "A false perception or belief; a deceptive appearance or impression."
+    },
+    {
+    word: "mysterious",
+    hint: "Difficult or impossible to understand, explain, or identify."
+    },
+    {
+    word: "whisper",
+    hint: "To speak very softly or quietly, often in a secretive manner."
+    },
+    {
+    word: "odd",
+    hint: "Not normally expected or expected; not in the usual or normal order."
+    },
+    {
+    word: "beautiful",
+    hint: "aesthetically pleasing and very gorgeous."
+    },
+    {
+    word: "ugly",
+    hint: "not beautiful or pleasing, unattractive"
+    },
+    {
+    word: "dream",
+    hint: "A long, imaginary, and often fantastical thought when sleeping"
+    },
+    {
+    word: "painting",
+    hint: "A picture or drawing made with paint etc."
+    },
+    {
+    word: "Taylor Swift",
+    hint: "biggest pop artist of all time."
+    },
+    {
+    word: "Grammy",
+    hint: "most prestigious award in music."
+    },
+    {
+    word: "bed",
+    hint: "A piece of furniture for sleeping."
+    },
+    {
+    word: "Cellphone",
+    hint: "Kids today spend too much time on these days."
+    },
+    {
+    word: "Piano",
+    hint: "A musical instrument with a keyboard."
+    },
+    {
+    word: "king",
+    hint: "a guy who rules over a large number of people."
+    },
+    {
+    word: "Card",
+    hint: "What people play poker with."
+    },
+    {
+    word: "Chair",
+    hint: "A piece of furniture for sitting."
+    },
+    {
+    word: "refrigerator",
+    hint: "A place to keep food fresh."
+    },
+    {
+    word: "toilet",
+    hint: "What do you flush?"
+    },
+    {
+    word: "book",
+    hint: "pages that are together."
+    },
+    {
+    word: "computer",
+    hint: "What device most people are on right now."
+    },
+    {
+    word: "water",
+    hint: "Humans can not survive three days without it."
+    }
+  ];
